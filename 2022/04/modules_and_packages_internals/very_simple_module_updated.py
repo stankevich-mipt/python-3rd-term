495560be70f6ed24bb1729d9e00becf44a98f2a9
Andrey Stankevich <stankevich.as@phystech.edu>
2022-10-10 15:31:30 +0300

    Added notebooks for seminars 4-6

--- 2022/04/modules_and_packages_internals/very_simple_module_updated.py
+++ 2022/04/modules_and_packages_internals/very_simple_module_updated.py
@@ -2,7 +2,7 @@ def foo():
     print("Our __name__ is:",  __name__)
     print("Foo is working")
 
-if __name__ == "__main__":
-    print("Our __name__ is:",  __name__)
-    print("Top level code is working")
-    foo()
+#if __name__ == "__main__":
+print("Our __name__ is:",  __name__)
+print("Top level code is working")
+foo()
